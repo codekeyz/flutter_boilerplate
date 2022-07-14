a057958e3997a0547c3d0c349c9a546db78f009e
Chima Precious <ositachima.co@gmail.com>
2022-07-14 09:28:33 +0000

    Feature/setup routing (#1)
    * add routing package
    
    * add homescreen
    
    * add and setup router
    
    * no need for tests atm
    
    * redesign homescreen
    
    * update ios config files

---
+++ lib/pages/home_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Future<void> _openYouTubeURL() async {
+    await launchUrl(Uri.parse('https://youtube.com/channel/UC6jlYIiB-_ckrAXBKsNRsvw'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      backgroundColor: Colors.grey.shade200,
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.network(
+              'https://drive.google.com/uc?export=download&id=1tqkpVzkwttJBfFDghTLfDowdOZ_cEfgc',
+              width: 400,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _openYouTubeURL,
+        tooltip: 'Open Link',
+        child: const Icon(Icons.open_in_browser),
+      ),
+    );
+  }
+}
