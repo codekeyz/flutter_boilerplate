a37cf592627deddf5935a4d574b73cb8c8774c1d
Chima Precious <ositachima.co@gmail.com>
2022-07-14 08:51:05 +0000

    add and setup router

---
+++ lib/router/user_router.dart
@@ -0,0 +1,14 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter_boilerplate/pages/home_screen.dart';
+
+@AdaptiveAutoRouter(
+  replaceInRouteName: 'Page,Route,Screen',
+  routes: <AutoRoute>[
+    AutoRoute(
+      path: '/',
+      name: "HomeRoute",
+      page: HomeScreen,
+    ),
+  ],
+)
+class $UserRouter {}
